382b835ab99d9141e9831d13ede6fc1bb3be6565
jcwreh <159987892+jcwreh@users.noreply.github.com>
2025-07-08 14:54:47 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-2_cQd4iu8Ts', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '5qaoe1k48iqna5dvl1sl8c53br', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
